fa3c7f68847923760907c934bf9a879ed8bde824
Edward <eddieim@gmail.com>
2020-08-03 12:50:02 -0600

    w1d1 and changed to data structures

---
+++ week_1/group1.js
@@ -0,0 +1,53 @@
+/* 
+  https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
+  Stable sort
+  
+  Time Complexity
+    - Best:     O(n) linear when array is already sorted
+    - Average:  O(n^2) quadratic
+    - Worst:    O(n^2) quadratic when the array is reverse sorted
+  Space: O(1) constant
+  For review, create a function that uses BubbleSort to sort an unsorted array in-place.
+  For every pair of adjacent indicies, swap them if the item on the left is larger than the item on the right until array is fully sorted
+*/
+
+function bubbleSort(nums){
+  var swap=false;
+  for(var i=0;i<arr.length;i++)
+  {
+      swap=false;
+      for(var j=1;j<arr.length;j++)
+      {
+        if(arr[j]<arr[j-1])
+        {
+            var temp=arr[j];
+             arr[j]=arr[j-1];
+             arr[j-1]=temp;
+             swap=true;
+        }   
+      }
+      if(!swap)
+          break;
+  }
+  return arr;
+
+}
+
+/* 
+  https://visualgo.net/en/sorting
+    
+  Selection sort works by iterating through the list, finding the minimum value, and moving it to the beginning of the list. Then, ignoring the first position, which is now sorted, iterate through the list again to find the next minimum value and move it to index 1. This continues until all values are sorted.
+  Unstable sort
+  
+  Time Complexity
+    - Best:     O(n^2) quadratic
+    - Average:  O(n^2) quadratic
+    - Worst:    O(n^2) quadratic
+  Space: O(1) constant
+  Selection sort is one of the slower sorts.
+  - ideal for: pagination, where page 1 displays 10 records for example, you run selection sort for 10 iterations only to display the first 10 sorted items
+*/
+
+function selectionSort(nums){
+
+}
