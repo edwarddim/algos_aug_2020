c0cc02f1c758d39d0fc6faca4562ef9a214ac7b8
Edward <eddieim@gmail.com>
2020-08-11 13:14:39 -0600

    w2d2

--- schedule_week2.js
+++ schedule_week2.js
@@ -88,7 +88,106 @@ class SLStack {
 // -----------------------------------------------------------------------------------------------//
 
 // TUE
-// FIFO - QUEUES 
+// FIFO - QUEUES
+
+class SLQueue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  // Time: O(1) constant
+  // Space: O(1)
+  isEmpty() {
+    return this.head === null;
+  }
+
+  // Time: O(1) constant
+  // Space: O(1)
+  enqueue(val) {
+    // ADD NODE TO THE BACK
+    // INCREMENT SIZE
+  }
+
+  // Time: O(1) constant
+  // Space: O(1)
+  dequeue() {
+    // REMOVE NODE FROM THE FRONT
+    // AND RETURN VALUE
+    // DECREMENT SIZE
+
+  }
+
+  // Time: O(1) constant
+  // Space: O(1)
+  front() {
+    // RETURN THE VALUE OF THE HEAD
+  }
+
+  // Time: O(n) linear
+  // Space: O(1)
+  contains(val) {
+    let runner = this.head;
+
+    while (runner) {
+      if (runner.val === val) return true;
+      runner = runner.next;
+    }
+    return false;
+  }
+
+  // Time: O(n) linear
+  // Space: O(n)
+  print() {
+    let runner = this.head;
+    let vals = "";
+
+    while (runner) {
+      vals += `${runner.data}${runner.next ? ", " : ""}`;
+      runner = runner.next;
+    }
+    console.log(vals);
+    return vals;
+  }
+
+  /* 
+    Queue: Is Palindrome
+    Output: Bool representing if the queue items are a palindrome (items same forwards as reversed)
+    
+    Restore Queue to original state before returning.
+    For storage, use one additional Stack only.
+    You CAN use vars to store a dequeued and/or popped item, 
+    but NO string concatenating the entire queue, no 2nd stack, 2nd queue, or arrays.
+    
+  */
+
+  /* 
+    Approach:
+    1. loop over fixed-length of queue
+      - dequeue each item and push it into the stack
+      - enqueue each item back into the queue to preserve it's order
+    2. loop over fixed-length of queue and check equality of popped & dequeued vals
+      - enqueue each dequeued item back into the queue to preserve it's order
+    Time: O(2n) -> O(n) linear, n = queue length
+    Space: O(n) from the stack being used
+  */
+
+  isPalindrome(){
+    
+  }
+
+  // Time: O(n) linear since enqueue is O(1), n = vals.length
+  // Space: O(1)
+}
+
+var queue = new SLQueue()
+queue.enqueue('a')
+queue.enqueue('b')
+queue.enqueue('c')
+queue.enqueue('b')
+queue.enqueue('a')
+queue.isPalindrome()
 
 // -----------------------------------------------------------------------------------------------//
 // -----------------------------------------------------------------------------------------------//
--- solutions_week2.js
+++ solutions_week2.js
@@ -85,3 +85,123 @@ class LinkedListStack {
       return vals;
     }
   }
+
+
+
+  class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+  
+  class LinkedListQueue {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
+    }
+  
+    // Time: O(1) constant
+    // Space: O(1)
+    isEmpty() {
+      return this.head === null;
+    }
+  
+    // Time: O(1) constant
+    // Space: O(1)
+    enqueue(val) {
+      const newTail = new Node(val);
+  
+      if (this.isEmpty()) {
+        this.head = newTail;
+        this.tail = newTail;
+      } else {
+        this.tail.next = newTail;
+        this.tail = newTail;
+      }
+      // pre-increment so the new size is returned otherwise old size is returned
+      return ++this.size;
+    }
+  
+    // Time: O(1) constant
+    // Space: O(1)
+    dequeue() {
+      // remove head
+      if (!this.head) {
+        return null;
+      }
+  
+      const dequeued = this.head;
+      this.head = this.head.next;
+  
+      if (this.head === null) {
+        this.tail = null;
+      }
+  
+      this.size--;
+      return dequeued.data;
+    }
+  
+    // Time: O(1) constant
+    // Space: O(1)
+    front() {
+      return this.head ? this.head.data : null;
+    }
+  
+    // Time: O(n) linear
+    // Space: O(1)
+    contains(val) {
+      let runner = this.head;
+  
+      while (runner) {
+        if (runner.val === val) return true;
+        runner = runner.next;
+      }
+      return false;
+    }
+  
+    // Time: O(n) linear
+    // Space: O(n)
+    print() {
+      let runner = this.head;
+      let vals = "";
+  
+      while (runner) {
+        vals += `${runner.data}${runner.next ? ", " : ""}`;
+        runner = runner.next;
+      }
+      console.log(vals);
+      return vals;
+    }
+  
+    // Time: O(n) linear since enqueue is O(1), n = vals.length
+    // Space: O(1)
+    seed(vals) {
+      vals.forEach((val) => this.enqueue(val));
+    }
+  }
+
+  isPalindrome() {
+    let isPalin = true;
+    const stack = new Stack(),
+      len = this.size();
+
+    for (let i = 0; i < len; i++) {
+      let dequeued = this.dequeue();
+      stack.push(dequeued);
+      this.enqueue(dequeued);
+    }
+
+    for (let i = 0; i < len; i++) {
+      let dequeued = this.dequeue();
+      let popped = stack.pop();
+
+      if (popped !== dequeued) {
+        isPalin = false;
+      }
+
+      this.enqueue(dequeued);
+    }
+    return isPalin;
+  }
